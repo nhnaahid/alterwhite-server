e77774a9143ed06b06da0a4338f17f6fcda40b08
nhnaahid <nhnahid.iu@gmail.com>
2024-06-22 17:50:11 +0600

    Deployment done

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const port = process.env.PORT || 5000;
 
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
+        "https://alterwhite-507f4.web.app",
+        "https://alterwhite-507f4.firebaseapp.com",
+    ],
+    credentials: true,
+}));
 app.use(express.json());
 
 
